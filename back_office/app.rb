2d6023219a6471f0d0ba98b7bc8ff23f49240a2c
Wade Xing <xingxing@tianji.com>
2015-05-08 16:46:03 +0800

    backoffice init

---
+++ back_office/app.rb
@@ -0,0 +1,26 @@
+require 'mongo'
+require 'sinatra'
+require 'mongo'
+require 'json/ext' # required for .to_json
+
+include Mongo
+
+configure do
+  conn = MongoClient.new("localhost", 27017)
+  set :mongo_connection, conn
+  set :mongo_db, conn.db('huo100')
+end
+
+get '/' do
+  content_type :json
+  settings.mongo_db.collection_names.to_json
+end
+
+post '/' do
+  sos = JSON.parse( request.body.read )
+  sos["GPS"]
+end
+
+get '/map' do
+  erb :map
+end
